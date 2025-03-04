8e7ecc768b76bf5d922c6981887120b81798e69e
Michelle Orozco <michelleoa1516@gmail.com>
2025-03-04 15:09:05 -0500

    Se agregó el primer ejercicio

---
+++ Cambio dinámico de contenido/app.js
@@ -0,0 +1,7 @@
+const boton = document.getElementById('miBoton');
+const texto = document.getElementById('texto');
+
+boton.addEventListener('click', function() {
+    texto.textContent = 'La moraleja de "Caperucita Roja" se centra en la importancia de no confiar en extraños. A pesar de que el lobo parecía amigable y usó palabras dulces, su intención era engañar a Caperucita, lo que resalta el mensaje de ser cautelosos y seguir las recomendaciones de los padres.';
+});
+

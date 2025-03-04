c08c48d8f65c86e95c46e72e109144b34d465a0d
Michelle Orozco <michelleoa1516@gmail.com>
2025-03-04 15:54:24 -0500

    Se agregó el segundo ejercicio

---
+++ Contador de clics/app.js
@@ -0,0 +1,9 @@
+let contadorClick = 0;
+
+const boton = document.getElementById('miBoton');
+const contadorTexto = document.getElementById('contador');
+
+boton.addEventListener('click', function(){
+    contadorClick++;
+    contadorTexto.value = contadorClick;
+});
